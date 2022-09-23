9a4f98fa0dc8b99a713430e0ec50805b2094cd2e
nnayk <79224511+nnayk@users.noreply.github.com>
2022-09-23 15:45:51 -0700

    added round-by-round gameplay

--- rps.js
+++ rps.js
@@ -2,8 +2,8 @@ function getComputerChoice()
 {
     const val = getMoveNum();
 
-    if (val==0) return "Rock";
-    else if (val==1) return "Paper";
+    if (val==0) return "rock";
+    else if (val==1) return "aper";
     else return "Scissors";
 }
 
@@ -12,4 +12,23 @@ function getMoveNum() {
     return Math.floor(Math.random() * NUM_MOVES);
 }
 
+function playRound(playerSelection,computerSelection)
+{
+    if (playerSelection==computerSelection) return "TIE!"
+    else if (playerSelection=="rock" && computerSelection=="paper" || 
+    playerSelection=="scissors" && computerSelection=="rock" || 
+    playerSelection=="paper" && computerSelection=="scissors") return "COMPUTER WINS!"
+    else return "you win."
+}
 
+
+function game()
+{
+    let playerMove="",computerMove="";
+    for(let i=0;i<5;i++)
+    {
+        playerMove=window.prompt("MOVE!!!").toLowerCase();
+        computerMove=getComputerChoice();
+        playRound(playerSelection,computerSelection);
+    }
+}
